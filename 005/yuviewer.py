1f010c02fe7f28c6bcd6011c0a62d4c1b9c93f58
francis <yinghao_liu@163.com>
2019-04-29 20:03:10 +0800

    add -s option, save jpg file

--- 005/yuviewer.py
+++ 005/yuviewer.py
@@ -10,12 +10,14 @@ def arg_parser():
     parser.add_argument("-i", "--input", required=True, help="input file(yuv)")
     parser.add_argument("-w", "--width", help="width, default=1920")
     parser.add_argument("-e", "--height", help="height, default=1080")
-    parser.add_argument("-c", "--color", help="color, now support only yuv420 format, include {}, default=yv12".format(g_color_map.keys()))
+    parser.add_argument("-c", "--color", help="color, now support only yuv420 format, include {}, default='YV12'".format(g_color_map.keys()))
+    parser.add_argument("-s", "--save", action='store_true', help="only save yuv to jpg")
     args=parser.parse_args()
     return args
 
 args = arg_parser()
 file_name = args.input
+save = args.save
 width = 1920 if None == args.width else args.width
 height = 1080 if None == args.height else args.height
 color = "YV12" if args.color not in g_color_map.keys() else args.color
@@ -30,8 +32,11 @@ if None == yuv_raw:
 
 np_yuv=np.array(list(yuv_raw), dtype=np.uint8).reshape(int(height*1.5), width)
 np_bgr=cv.cvtColor(np_yuv, g_color_map[color])
-cv.imshow("YUV", np_yuv)
-cv.imshow("BGR", np_bgr)
-cv.waitKey()
+if save:
+    cv.imwrite(file_name.rstrip("yuv")+"jpg", np_bgr)
+else:
+    cv.imshow("YUV", np_yuv)
+    cv.imshow("BGR", np_bgr)
+    cv.waitKey()
 
 
